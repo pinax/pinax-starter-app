cc57a139dff2bc654c9a823a692c84c6c6916dd5
Patrick Altman <paltman@gmail.com>
2015-09-10 22:38:25 -0500

    Fix typo

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     name="pinax-{{ app_name }}",
     long_description=read("README.rst"),
     version="0.1",
-    url="http://github.com/pinax/pianx-{{ app_name }}/",
+    url="http://github.com/pinax/pinax-{{ app_name }}/",
     license="MIT",
     packages=find_packages(),
     package_data={
